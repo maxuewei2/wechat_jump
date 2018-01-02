09d6908275fd43fc4e8ca8bf29357c2a3fdf4e68
maxuewei2 <maxuewei1995@126.com>
2018-01-03 00:56:18 +0800

    加速，减少了一半的颜色比较次数 	修改：     wechat_jump.py

--- wechat_jump.py
+++ wechat_jump.py
@@ -29,12 +29,15 @@ def is_equal(a, b):
 
 
 def get_man_point(rgb_im):
-    bias_nums = [0, 35, 31, 60, 58]
+    bias_nums = [35, 31, 60, 58]
     for y in range(700, Y):
-        flag = False
         for x in range(X):
             bias = 0
+            color = rgb_im.getpixel((x, y))
+            if distance(color, man_colors[0]) > 50:
+            	continue
             colors = []
+            colors.append(color)
             for bias_n in bias_nums:
                 bias += bias_n
                 if y + bias >= Y:
@@ -42,14 +45,10 @@ def get_man_point(rgb_im):
                 colors.append(rgb_im.getpixel((x, y + bias)))
             if is_equal(colors, man_colors):
                 y = y + bias - 9
-                flag = True
-                break
-        if flag:
-            break
-    if not flag:
-        print('没能找到小人位置，请手动跳一次然后再次运行本程序')
-        exit(0)
-    return (x, y)
+                return (x, y)
+    print('没能找到小人位置，请手动跳一次然后再次运行本程序')
+    exit(0)
+    
 
 
 def get_highest_point(rgb_im, man_point):
@@ -75,7 +74,7 @@ if __name__ == '__main__':
     scs = [x for x in scs if x.startswith('sc_')]
     scs = [x[3:-4] for x in scs]
     scs = [int(x) for x in scs]
-    start_num = max(scs) + 1
+    start_num = max(scs) + 1 if scs else 0
     print('screenshot file name starts from ', start_num)
     for i in range(10000):
         print('第 %d 跳' %(i+1))
@@ -89,6 +88,6 @@ if __name__ == '__main__':
         print('最高点:\t', highest_p)
         dest_p = get_dest_point(man_p, center_point, highest_p[0])
         print('目标点:\t' ,dest_p)
-        swipe_time = int(math.sqrt((man_p[0] - dest_p[0])**2 + (man_p[1] - dest_p[1])**2) * 1.363)
-        os.system('adb shell input swipe 500 500 500 500 ' + str(swipe_time))
+        press_time = int(math.sqrt((man_p[0] - dest_p[0])**2 + (man_p[1] - dest_p[1])**2) * 1.363)
+        os.system('adb shell input swipe 500 500 500 500 ' + str(press_time))
         time.sleep(2)
